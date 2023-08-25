f1965c8c5436437b1d9ad61c40701a6eb1d51d86
John Vundi <johnsonvundy2@gmail.com>
2023-08-25 10:16:23 +0300

    feat: export item card component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
+import { ElewaNewsItemCardComponent } from './elewa-news-item-card/elewa-news-item-card.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaInfoCardComponent],
-  exports: [ElewaInfoCardComponent]
+  declarations: [ElewaInfoCardComponent, ElewaNewsItemCardComponent],
+  exports: [ElewaInfoCardComponent, ElewaNewsItemCardComponent],
 })
 export class CardsModule {}
