10a9a2ccd8a276aeb3ca2b45b92660ef8404a630
John Vundi <johnsonvundy2@gmail.com>
2023-08-28 23:46:16 +0300

    feat: imported the partnerSectioncomponent and configured functionality

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
-import { PartnersSectionComponent } from './main/home/components/partners-section/partners-section.component';
+import { PartnersSectionComponent } from './components/partners-section/partners-section.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
   declarations: [HomePageComponent, PartnersSectionComponent],
-  exports: [HomePageComponent],
+  exports: [HomePageComponent, PartnersSectionComponent],
 })
 export class FeaturesPagesHomeModule {}
